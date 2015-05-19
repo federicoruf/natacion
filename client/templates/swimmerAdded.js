f7847fd0feee1af7d281c850e8685f6752140d59
federico rufrancos <fde.ruf@gmail.com>
2015-05-19 10:49:24 -0300

    agregado bootboxjs

--- client/templates/swimmerAdded.js
+++ client/templates/swimmerAdded.js
@@ -4,6 +4,19 @@ Template.swimmerAdded.helpers({
   }
 });
 
-bootbox.alert("Hello world!", function() {
-  Example.show("Hello world callback");
+Template.swimmerAdded.events({
+    "click .infoNadador": function(){
+        var nadador ={
+            nombre: this.nombre
+        };
+        bootbox.alert("Nombre y apellido: <strong>" + this.nombre + " " + this.apellido + "</strong><br/>" + 
+                      " DNI: <strong>" + this.dni + "</strong><br/>" + 
+                      " Fecha de nacimiento: <strong>" + this.fechaDeNacimiento + "</strong><br/>" +  
+                      " Ciudad: <strong>" + this.ciudad + "</strong><br/>" + 
+                      " Club: <strong>" + this.club + "</strong><br/>" + 
+                      " Obra social: <strong>" + this.obraSocial + "</strong><br/>" + 
+                      " Email: <strong>" + this.email + "</strong><br/>" + 
+                      " Sexo: <strong>" + this.sexo + "</strong><br/>" + 
+                      "FOTO!!!");
+    }
 });
