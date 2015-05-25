b94ee4944563b432c73829a7abcd5f5bd677fd8b
Mauro89x <mauro.a89x@gmail.com>
2015-05-25 17:16:45 -0300

    En el listado de nadadores, si seleccionas se pinta de color amarillo, refactoring sobre algunas partes del swimmerAdded.html, ver swimmer_info.html, recordar que los nombres de los archivos, segun el manual de discovermeteor deben ser swimmer_added.html usar guion bajo no lower camelcase

--- client/templates/swimmers/swimmerAdded.js
+++ client/templates/swimmers/swimmerAdded.js
@@ -1,7 +1,9 @@
 Template.swimmerAdded.helpers({
   nadadores: function() {
+  
     return Nadadores.find();
   }
+
 });
 
 Template.swimmerAdded.events({
@@ -9,14 +11,14 @@ Template.swimmerAdded.events({
         var imageDB = Images.findOne(this.foto._id);
         var nombre = imageDB.original.name;
         var id = imageDB._id;
-        bootbox.alert("Nombre y apellido: <strong>" + this.nombre + " " + this.apellido + "</strong><br/>" + 
-                      " DNI: <strong>" + this.dni + "</strong><br/>" + 
-                      " Fecha de nacimiento: <strong>" + this.fechaDeNacimiento + "</strong><br/>" +  
-                      " Ciudad: <strong>" + this.ciudad + "</strong><br/>" + 
-                      " Club: <strong>" + this.club + "</strong><br/>" + 
-                      " Obra social: <strong>" + this.obraSocial + "</strong><br/>" + 
-                      " Email: <strong>" + this.email + "</strong><br/>" + 
-                      " Sexo: <strong>" + this.sexo + "</strong><br/>" + 
+        bootbox.alert("Nombre y apellido: <strong>" + this.nombre + " " + this.apellido + "</strong><br/>" +
+                      " DNI: <strong>" + this.dni + "</strong><br/>" +
+                      " Fecha de nacimiento: <strong>" + this.fechaDeNacimiento + "</strong><br/>" +
+                      " Ciudad: <strong>" + this.ciudad + "</strong><br/>" +
+                      " Club: <strong>" + this.club + "</strong><br/>" +
+                      " Obra social: <strong>" + this.obraSocial + "</strong><br/>" +
+                      " Email: <strong>" + this.email + "</strong><br/>" +
+                      " Sexo: <strong>" + this.sexo + "</strong><br/>" +
                       "<img src=/cfs/files/images/" + id + '/'+ nombre + " alt='imagen del nadador' class='thumbnail' height='200' width='260'/>" +                       " Información adicional: datos sobre resultados en competencias anteriores");
     }
 });
