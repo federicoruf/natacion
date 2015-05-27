51d5932aa3ca8ad096a06ce3ea2e6d75c1cdd9f5
federico rufrancos <fde.ruf@gmail.com>
2015-05-27 14:48:47 -0300

    fin de edición de AMB de categorías
    se agrego la funcionalidad para modificar los años

--- client/templates/categories/category_administration.js
+++ client/templates/categories/category_administration.js
@@ -3,3 +3,33 @@ Template.categoryAdministration.helpers({
         return Categories.find();
     }
 });
+
+
+Template.categoryAdministration.events({
+  "click .upYear": function () {
+    var categories = Categories.find().fetch();
+    categories.forEach(function (category) {
+      category.yearStart++; 
+      category.yearFinish++; 
+      Meteor.call('yearsUpdate', category, function(error, result) {
+        if (error) {
+          return alert(error.reason);
+        } 
+      });
+    });
+  },
+  
+  "click .downYear": function () {
+    var categories = Categories.find().fetch();
+    categories.forEach(function (category){
+       category.yearStart--;
+       category.yearFinish--; 
+       Meteor.call('yearsUpdate', category, function(error, result) {
+        if (error) {
+          return alert(error.reason);
+        } 
+      });
+    });
+  }
+});
+
