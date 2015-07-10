1b312f541a8feb206426cca5d90e3b46956d426e
federico rufrancos <fde.ruf@gmail.com>
2015-07-10 19:46:34 -0300

    boton de edición
    se corrigio el botón de edición y se movía a la sección de info de
    nadador

--- client/templates/swimmers/swimmer_added.js
+++ client/templates/swimmers/swimmer_added.js
@@ -4,15 +4,41 @@ Template.swimmerAdded.helpers({
   },
   
   myCollection: function () {
+    i18n.setLanguage('es');
     return Nadadores;
+  },
+  
+  tableSettings: function() {
+    return {
+      fields: [
+        { key: 'nombre', label: 'Nombre' },
+        { key: 'apellido', label: 'Apellido' },
+        { key: 'dni', label: 'Dni' },
+        { key: 'obraSocial', label: 'Obra Social' },
+        { key: 'club', label: 'Club' },
+        { key: 'ciudad', label: 'Ciudad' },
+        { key: 'email', label: 'Email' },
+        { key: 'sexo', label: 'Sexo' },
+        { key: 'numero', label: 'Número' },
+        { key: 'fechaDeNacimiento', label: 'Fecha de nacimiento' },
+      ]
+    }
   }
 });
 
+
 Template.swimmerAdded.events({
-  "click .infoNadador": function(){
+  'click .reactive-table tr': function(){
     var imageDB = Images.findOne(this.foto._id);
     var nombre = imageDB.original.name;
     var id = imageDB._id;
+    
+    var currentUser = Meteor.user();
+    var editar = '';
+    if (Roles.userIsInRole(currentUser, 'director') || Roles.userIsInRole(currentUser, 'assistantDirector')) {
+      editar = '<a href="/nadadores/' + this._id + '/edit" onclick="bootbox.hideAll()">Editar</a>';
+    }
+    
     bootbox.alert("Nombre y apellido: <strong>" + this.nombre + " " + this.apellido + "</strong><br/>" +
                   " DNI: <strong>" + this.dni + "</strong><br/>" +
                   " Fecha de nacimiento: <strong>" + this.fechaDeNacimiento + "</strong><br/>" +
@@ -21,6 +47,8 @@ Template.swimmerAdded.events({
                   " Obra social: <strong>" + this.obraSocial + "</strong><br/>" +
                   " Email: <strong>" + this.email + "</strong><br/>" +
                   " Sexo: <strong>" + this.sexo + "</strong><br/>" +
-                  "<img src=/cfs/files/images/" + id + '/'+ nombre + " alt='imagen del nadador' class='thumbnail' height='200' width='260'/>" +                       " Información adicional: datos sobre resultados en competencias anteriores");
+                  "<img src=/cfs/files/images/" + id + '/'+ nombre + " alt='imagen del nadador' class='thumbnail' height='200' width='260'/>" +                       
+                  " Información adicional: datos sobre resultados en competencias anteriores <br/>" +
+                 editar);
   }
 });
